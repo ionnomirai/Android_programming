6d368f76117219316bab2069cc17e65a3a46009d
ionnomirai <kindbakaneko@gmail.com>
2024-01-11 11:18:35 -0800

    A new module Options_menu has been created
    * viewBinding connected;
    * added icons;
    * created and applied background for TextView;
    * added string resources and color resources

---
+++ options_menu_n00/src/main/java/com/example/options_menu_n00/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.options_menu_n00
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.options_menu_n00.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Android_programming"
 include(":app")
+include(":options_menu_n00")
